e6724a3812a68d69862cc528f9eb61a02d27cddf
FrancescaLai <francescalai1690@gmail.com>
2021-07-13 13:25:33 +0200

    Add: new msg real timing
    Script has been included

--- js/script.js
+++ js/script.js
@@ -10,19 +10,19 @@ var app = new Vue(
           visible: true,
           messages: [
             {
-              time: '15:30:55',
+              time: '15:30',
               message: 'Hai portato a spasso il cane?',
               status: 'sent',
               active: 'false'
             },
             {
-              time: '15:50:00',
+              time: '15:36',
               message: 'Ricordati di dargli da mangiare',
               status: 'sent',
               active: 'false'
             },
             {
-              time: '16:15:22',
+              time: '16:15',
               message: 'Tutto a posto, non ti preoccupare!',
               status: 'received',
               active: 'false'
@@ -36,19 +36,19 @@ var app = new Vue(
           visible: true,
           messages: [
             {
-              time: '16:30:00',
+              time: '16:30',
               message: 'Ciao come stai?',
               status: 'sent',
               active: 'false'
             },
             {
-              time: '16:30:55',
+              time: '16:30',
               message: 'Bene grazie! Stasera ci vediamo?',
               status: 'received',
               active: 'false'
             },
             {
-              time: '16:35:00',
+              time: '16:35',
               message: 'Mi piacerebbe ma devo andare a fare la spesa.',
               status: 'received',
               active: 'false'
@@ -62,19 +62,19 @@ var app = new Vue(
           visible: true,
           messages: [
             {
-              time: '10:10:40',
+              time: '10:10',
               message: 'La Marianna va in campagna',
               status: 'received',
               active: 'false'
             },
             {
-              time: '10:20:10',
+              time: '10:20',
               message: 'Sicuro di non aver sbagliato chat?',
               status: 'sent',
               active: 'false'
             },
             {
-              time: '16:15:22',
+              time: '16:15',
               message: 'Ah scusa!',
               status: 'received',
               active: 'false'
@@ -88,13 +88,13 @@ var app = new Vue(
           visible: true,
           messages: [
             {
-              time: '15:30:55',
+              time: '15:30',
               message: 'Lo sai che ha aperto una nuova pizzeria?',
               status: 'sent',
               active: 'false'
             },
             {
-              time: '15:50:00',
+              time: '15:50',
               message: 'Si, ma preferirei andare al cinema',
               status: 'received',
               active: 'false'
@@ -118,7 +118,7 @@ var app = new Vue(
 
       addNewMessage: function(){
         let text = {
-          date: '10/01/2020 15:50:00',
+          time: dayjs().format('HH:mm'),
           message: this.newMessage,
           status: "sent"
         };
@@ -131,7 +131,7 @@ var app = new Vue(
 
       setTimeout(function(){
         let text = {
-          date: '10/01/2020 15:50:00',
+          date: '10/01/2020 15:50',
           message: "ok",
           status: "received"
         };
